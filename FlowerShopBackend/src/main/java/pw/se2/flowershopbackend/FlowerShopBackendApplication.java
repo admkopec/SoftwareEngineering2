fdb2f7dc6b8d30f781d3db4d2b425539982d626e
Adam Kopeć <adamkopec5@icloud.com>
2023-03-16 20:45:17 +0100

    Project Setup

---
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/FlowerShopBackendApplication.java
@@ -0,0 +1,13 @@
+package pw.se2.flowershopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerShopBackendApplication.class, args);
+    }
+
+}
---
+++ FlowerShopBackend/src/test/java/pw/se2/flowershopbackend/FlowerShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.se2.flowershopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
