0641ffbccc5f0caed698dd992852f42f505205ed
szmarkiewicz <markiewiczszym@gmail.com>
2023-03-20 23:09:53 +0100

    Updated config for Cypress/TS

--- flower-shop-frontend/src/components/SignUpComponent.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SignUpComponent from './SignUpComponent'
-
-describe('<SignUpComponent />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<SignUpComponent />)
-  })
-})
