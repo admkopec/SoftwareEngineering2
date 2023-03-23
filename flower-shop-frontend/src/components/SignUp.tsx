6342f2f804b3b7a0f4ac633d699d6776c82e5e2b
AlexFelicytaNovak <115529759+AlexFelicytaNovak@users.noreply.github.com>
2023-03-23 10:50:43 +0100

    added sign up component and confirmation (#5)
    * added sign up component and confirmation
    
    * added SignUp component validation

---
+++ flower-shop-frontend/src/components/SignUp.tsx
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { ThemeProvider } from '@mui/material/styles';
+import Copyright from './Copyright';
+import { mainTheme } from './Themes'
+import delay from '../util/delay';
+import { useNavigate } from 'react-router-dom';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function SignUp() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    setIsLoading(true);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    await delay(2000);
+    navigate('/signup/success');
+    setIsLoading(false);
+  };
+
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="hasNewsletter" color="primary" />}
+                  label="I would like to partake in the Newsletter programme, that is receive discount information, updates and suggestions via email."
+                />
+              </Grid>
+            </Grid>
+            <LoadingButton
+              loading={isLoading}
+              loadingIndicator={<CircularProgress color='primary' size={20} />}
+              type="submit"
+              variant="contained"
+              sx={{ mt: 3, mb: 2, float: 'right'}}
+            >
+              Sign Up
+            </LoadingButton>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Log In
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ flower-shop-frontend/src/components/Themes.tsx
@@ -0,0 +1,65 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    palette?: {
+      primary?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      secondary?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      error?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      warning?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      info?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      custom?: {
+        light?: string,
+        main?: string,
+        dark?: string,
+        contrastText?: string,
+      },
+      contrastThreshold?: number,
+      tonalOffset?: number,
+    },
+  }
+}
+
+export const mainTheme = createTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#054b65',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#0066ff',
+      main: '#0044ff',
+      // dark: will be calculated from palette.secondary.main,
+      contrastText: '#ffcc00',
+    },
+    contrastThreshold: 4.5,
+    tonalOffset: 0.2,
+  },
+});
