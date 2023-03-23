bb6295d0296ae416aebda1c437f1cc70dc2f9396
Szymon Markiewicz <103886215+szmarkiewicz@users.noreply.github.com>
2023-03-23 11:37:40 +0100

    Add tests and LogIn component (#3)
    * Create main.yml
    
    * Update config
    
    * Add cypress configuration file
    
    * Updated config for Cypress/TS
    
    * Fix TS config
    
    * Add simple test for App component
    
    * Config for Cypress types
    
    * Config update
    
    * Add Cypress config file
    
    * Config for Jest support
    
    * Setup tests for Jest
    
    * Setup Jest workflow
    
    * Update workflow file
    
    * Set wd
    
    * Fix wd
    
    * Update Jest workflow
    
    * Update workflow
    
    * Update npm cache dir
    
    * Add app template
    
    * Add Log In component with POST request
    
    * Add jest tests
    
    * Fix naming
    
    * Finalise tests
    
    * Update paths
    
    * Fix paths again
    
    * Fix path in coverage

---
+++ flower-shop-frontend/src/tests/SignUpSuccess.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import SignUpSuccess from '../components/SignUpSuccess';
+
+describe('<SignUpSuccess /> ', () => {
+    test('contains proper text', () => {
+        render(<SignUpSuccess />);
+        const heading = screen.getAllByRole('heading');
+        expect(heading).toHaveLength(2);
+        expect(heading[0]).toHaveTextContent('You have been successfully signed up!');
+        expect(heading[1]).toHaveTextContent('You may log in now.');
+    });
+});
