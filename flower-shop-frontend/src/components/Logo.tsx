dbc805805c2a9a8a44291ce2539121b71bb5e249
szmarkiewicz <markiewiczszym@gmail.com>
2023-04-06 09:08:16 +0200

    Add Logo and tests

--- flower-shop-frontend/src/components/Logo.tsx
+++ flower-shop-frontend/src/components/Logo.tsx
@@ -5,7 +5,7 @@ import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
 export default function Logo() {
   return (
     <Box sx={{ display: 'flex', p: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-      <LocalFloristIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+      <LocalFloristIcon titleAccess='flower-icon' sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
       <Typography
         variant="h6"
         noWrap
