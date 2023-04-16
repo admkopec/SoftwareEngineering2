d00ef88059e6342a97e42863879c9d9fcee7b36d
szmarkiewicz <markiewiczszym@gmail.com>
2023-04-16 12:39:29 +0200

    Fix user creation

--- FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/UserCreationDto.java
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/UserCreationDto.java
@@ -6,12 +6,16 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 // TODO: Maybe add account type, address, etc.?
-public record UserCreationDto(@NotEmpty String name, @Email String email, @NotEmpty String password) {
+public record UserCreationDto(@NotEmpty String name, @NotEmpty @Email String email, @NotEmpty String password,
+                              @NotEmpty boolean newsletter) {
     public User convertToModel() {
         User user = new User();
         user.setName(this.name());
         user.setEmail(this.email());
         user.setPassword(this.password());
+        user.setRole(User.Roles.Client);
+        user.setNewsletter(this.newsletter());
+
         // TODO: Set other properties
         return user;
     }
