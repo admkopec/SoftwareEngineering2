ab77cf15a1fe65afbb49a5f9384e4ceeb4523c4c
szmarkiewicz <markiewiczszym@gmail.com>
2023-04-16 18:03:26 +0200

    Update product card

--- flower-shop-frontend/src/components/ProductCard.tsx
+++ flower-shop-frontend/src/components/ProductCard.tsx
@@ -5,31 +5,39 @@ import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 import React from 'react';
+import { Product } from '../resources/types';
+import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 
-export default function ProductCard() {
+interface ProductCardProps{
+  product: Product
+}
+
+export default function ProductCard(props: ProductCardProps) {
   return (
       <Grid item style={{width: '250px'}}>
-    <Card variant="outlined">
-      <CardMedia
-        component="img"
-        alt="roses bouquet"
-        height="300"
-        width="400"
-        image={require('../static/imgs/rose-bouquet.jpg')}
-      />
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Roses
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Bouquet
-        </Typography>
-        <Typography variant="body2">Very beautiful flower!</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Go to Product</Button>
-      </CardActions>
-    </Card>
+        <Card variant="outlined">
+          <CardMedia
+            component="img"
+            alt="roses bouquet"
+            height="300"
+            width="400"
+            image={require('../static/imgs/rose-bouquet.jpg')}
+          />
+          <CardContent>
+            <Typography variant="h5" component="div" role='product name'>
+              {props.product.name}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              {props.product.category}
+            </Typography>
+            <Typography variant="body2">
+              {props.product.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" endIcon={<KeyboardArrowRightRoundedIcon />}>Go to Product </Button>
+          </CardActions>
+        </Card>
       </Grid>
   );
 }
