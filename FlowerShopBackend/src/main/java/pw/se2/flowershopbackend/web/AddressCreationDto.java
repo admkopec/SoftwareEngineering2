32eabca698843ebcc8de3aedc36b2253aa5c06f1
szmarkiewicz <markiewiczszym@gmail.com>
2023-04-18 22:41:00 +0200

    Proposition for extension of AddressDto

---
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/AddressCreationDto.java
@@ -0,0 +1,8 @@
+package pw.se2.flowershopbackend.web;
+
+import javax.validation.constraints.NotEmpty;
+
+public record AddressCreationDto(@NotEmpty String street, @NotEmpty String buildingNo, @NotEmpty String houseNo,
+                                 @NotEmpty String postalCode, @NotEmpty String city, @NotEmpty String country) {
+
+}
--- FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/AddressDto.java
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/AddressDto.java
@@ -2,5 +2,6 @@ package pw.se2.flowershopbackend.web;
 
 import javax.validation.constraints.NotEmpty;
 
-public record AddressDto(@NotEmpty String street, @NotEmpty String city, @NotEmpty String postalCode, @NotEmpty String country) {
+public record AddressDto(@NotEmpty String street, @NotEmpty String buildingNo, @NotEmpty String houseNo,
+                         @NotEmpty String postalCode, @NotEmpty String city, @NotEmpty String country) {
 }
