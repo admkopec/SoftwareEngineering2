e2406a18a6d9ef820928f3043952400dd990d602
szmarkiewicz <markiewiczszym@gmail.com>
2023-04-21 06:40:57 +0200

    Change response key (jwtToken) to conform to frontend

--- FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/security/web/JwtResponse.java
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/security/web/JwtResponse.java
@@ -3,7 +3,7 @@ package pw.se2.flowershopbackend.security.web;
 import java.io.Serial;
 import java.io.Serializable;
 
-public record JwtResponse(String jwttoken) implements Serializable {
+public record JwtResponse(String jwtToken) implements Serializable {
     @Serial
     private static final long serialVersionUID = -8091879091924046844L;
 }
