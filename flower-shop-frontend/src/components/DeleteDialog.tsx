bef88f1788a0e9277ff266473b15e2d690ec27b9
Adam Kopeć <adamkopec5@icloud.com>
2023-04-25 17:43:01 +0200

    Made some changes to linting Fixed some linting errors but the linter is still not passing...

--- flower-shop-frontend/src/components/DeleteDialog.tsx
+++ flower-shop-frontend/src/components/DeleteDialog.tsx
@@ -5,73 +5,71 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {Product} from "../resources/types";
-import {IS_DEV} from "../resources/constants";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Product } from '../resources/types';
+import { IS_DEV } from '../resources/constants';
 
-interface DeleteDialogProps{
-    productId: string | undefined,
-    productName: string | undefined,
-    open: boolean,
-    setOpen: (isOpen: boolean) => void,
+interface DeleteDialogProps {
+  productId: string | undefined;
+  productName: string | undefined;
+  open: boolean;
+  setOpen: (isOpen: boolean) => void;
 }
 
 export default function DeleteDialog(props: DeleteDialogProps) {
-    const [deleteButtonText, setDeleteButtonText] = useState<string>('Delete');
-    const navigate = useNavigate();
-    const handleCancel = () => {
-        props.setOpen(false);
-    };
+  const { productId, productName, open, setOpen } = props;
+  const [deleteButtonText, setDeleteButtonText] = useState<string>('Delete');
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    setOpen(false);
+  };
 
-    const handleDelete = async () => {
-        await fetch(`/api/products/${props.productId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`,
-            }})
-            .then((response) => {
-                if (response.ok) return response.json();
-                throw new Error(`ERROR ${response.status}`);
-            })
-            .then((responseJSON: Product) => {
-                IS_DEV && console.log('Success deleting product.');
-                IS_DEV && console.log(responseJSON);
-            })
-            .catch((e) => {
-                IS_DEV && console.log(`Error when trying to delete product: ${e}`);
+  const handleDelete = async () => {
+    await fetch(`/api/products/${productId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('jwtToken') ?? ''}`
+      }
+    })
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error(`ERROR ${response.status}`);
+      })
+      .then((responseJSON: Product) => {
+        IS_DEV && console.log('Success deleting product.');
+        IS_DEV && console.log(responseJSON);
+      })
+      .catch((e) => {
+        IS_DEV && console.log(`Error when trying to delete product: ${e.toString()}`);
+      });
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+      <DialogTitle id="alert-dialog-title">{`Delete item "${productName}?`}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          This action cannot be undone. {productName} will be permanently deleted and will not be available to online
+          customers.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel}>Cancel</Button>
+        <Button
+          onClick={() => {
+            setDeleteButtonText('Deleting...');
+            handleDelete().then(() => {
+              navigate(-1);
             });
-        props.setOpen(false);
-    };
-
-    return (
-        <>
-            <Dialog
-                open={props.open}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    {`Delete item "${props.productName}?`}
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        This action cannot be undone. {props.productName} will be permanently deleted and will not be available to
-                        online customers.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCancel}>Cancel</Button>
-                    <Button onClick={(event) => {
-                        setDeleteButtonText('Deleting...');
-                        handleDelete().finally();
-                        navigate(-1);
-                    }} autoFocus>
-                        {deleteButtonText}
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </>
-    );
+          }}
+          autoFocus
+        >
+          {deleteButtonText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
--- flower-shop-frontend/src/components/Footer.tsx
+++ flower-shop-frontend/src/components/Footer.tsx
@@ -1,101 +1,104 @@
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Grid from '@mui/material/Grid'
-import {Stack} from '@mui/material';
-import * as React from "react";
-import {alpha, styled} from "@mui/material/styles";
-import {mainTheme as theme} from "../resources/themes";
-import {Divider} from "@mui/material";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { Stack, Divider } from '@mui/material';
+import * as React from 'react';
+import { alpha, styled } from '@mui/material/styles';
 import LocalPhone from '@mui/icons-material/LocalPhone';
 import Email from '@mui/icons-material/Email';
 import Facebook from '@mui/icons-material/Facebook';
 import Instagram from '@mui/icons-material/Instagram';
-import Copyright from "./Copyright";
+import { mainTheme as theme } from '../resources/themes';
+import Copyright from './Copyright';
 
 const FooterBox = styled(Box)(() => ({
-    margin: theme.spacing('4%', 0, 0, 0),
-    padding: theme.spacing('2%', 0, '2%', 0),
-    width: '100%',
-    height: '30%',
-    position: 'relative',
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'nowrap',
-    justifyContent: 'center',
-    alignItems:'center',
-    bottom: 0,
-    backgroundColor: alpha(theme.palette?.primary?.light as string, 1)
-
+  margin: theme.spacing('4%', 0, 0, 0),
+  padding: theme.spacing('2%', 0, '2%', 0),
+  width: '100%',
+  height: '30%',
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  flexWrap: 'nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  bottom: 0,
+  backgroundColor: alpha(theme.palette?.primary?.light as string, 1)
 }));
 
 const OptionsBox = styled(Box)(() => ({
-    color:'white',
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    alignContent:'center',
+  color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignContent: 'center'
 }));
 
 const FooterGrid = styled(Grid)(() => ({
-    color: 'white',
-    fontSize: 13,
-    textAlign: 'center',
-    width: '30%',
-    justifyContent:'center',
-
-
+  color: 'white',
+  fontSize: 13,
+  textAlign: 'center',
+  width: '30%',
+  justifyContent: 'center'
 }));
 
-export default function Footer(){
-    return (
+export default function Footer() {
+  return (
     <FooterBox>
-        <FooterGrid container  >
-            <Grid item xs>
-                <Stack spacing={2} >
-                    <Typography variant="caption" component="a" fontSize="13">
-                        CONTACT US
-                    </Typography>
-                    <OptionsBox >
-                        <Typography sx={{pr: 1}} variant="caption" component="a" >
-                            PHONE
-                         </Typography>
-                        <LocalPhone/>
-                    </OptionsBox>
-                    <OptionsBox >
-                        <Typography sx={{pr: 1}} variant="caption" component="a" >
-                            MAIL
-                        </Typography>
-                        <Email/>
-                    </OptionsBox>
-                </Stack>
-            </Grid>
-            <Divider orientation="vertical" color='white' flexItem/>
-            <Grid item xs>
-                <Stack spacing={2} >
-                    <Typography  variant="caption" component="a" >
-                        FOLLOW US
-                    </Typography>
-                    <OptionsBox>
-                        <Typography sx={{pr: 1, textDecoration: 'none'}} color='white' variant="caption" component="a" href="https://www.facebook.com/" >
-                            FACEBOOK
-                        </Typography>
-                        <Facebook/>
-                    </OptionsBox>
-                    <OptionsBox>
-                        <Typography sx={{pr: 1, textDecoration: 'none'}} color='white' variant="caption" component="a" href="https://www.instagram.com/" >
-                            INSTAGRAM
-                        </Typography>
-                        <Instagram/>
-                    </OptionsBox>
-                </Stack>
-            </Grid>
-        </FooterGrid>
-        <Copyright />
+      <FooterGrid container>
+        <Grid item xs>
+          <Stack spacing={2}>
+            <Typography variant="caption" component="a" fontSize="13">
+              CONTACT US
+            </Typography>
+            <OptionsBox>
+              <Typography sx={{ pr: 1 }} variant="caption" component="a">
+                PHONE
+              </Typography>
+              <LocalPhone />
+            </OptionsBox>
+            <OptionsBox>
+              <Typography sx={{ pr: 1 }} variant="caption" component="a">
+                MAIL
+              </Typography>
+              <Email />
+            </OptionsBox>
+          </Stack>
+        </Grid>
+        <Divider orientation="vertical" color="white" flexItem />
+        <Grid item xs>
+          <Stack spacing={2}>
+            <Typography variant="caption" component="a">
+              FOLLOW US
+            </Typography>
+            <OptionsBox>
+              <Typography
+                sx={{ pr: 1, textDecoration: 'none' }}
+                color="white"
+                variant="caption"
+                component="a"
+                href="https://www.facebook.com/"
+              >
+                FACEBOOK
+              </Typography>
+              <Facebook />
+            </OptionsBox>
+            <OptionsBox>
+              <Typography
+                sx={{ pr: 1, textDecoration: 'none' }}
+                color="white"
+                variant="caption"
+                component="a"
+                href="https://www.instagram.com/"
+              >
+                INSTAGRAM
+              </Typography>
+              <Instagram />
+            </OptionsBox>
+          </Stack>
+        </Grid>
+      </FooterGrid>
+      <Copyright />
     </FooterBox>
-    );
+  );
 }
-
-
-
-
-
