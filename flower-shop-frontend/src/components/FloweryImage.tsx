33973545828a5b1304cd47a6338b0bd430f7b68a
Adam Kopeć <adamkopec5@icloud.com>
2023-04-26 15:33:55 +0200

    Frontend linting updates (#38)
    * Made some changes to linting
    Fixed some linting errors but the linter is still not passing...
    
    * Updated linter to use reviewdog
    
    * Fixed permissions for reviewdog
    
    * Fixed contents permission
    
    * Prevented eslint from running in build script

--- flower-shop-frontend/src/components/FloweryImage.tsx
+++ flower-shop-frontend/src/components/FloweryImage.tsx
@@ -1,17 +1,14 @@
-import Image from "mui-image";
-import * as React from "react";
-import {useState} from "react";
+import Image from 'mui-image';
+import * as React from 'react';
+import { useState } from 'react';
 
-interface FloweryImageProps{
-  src: string,
-  width: string,
-  height: string
+interface FloweryImageProps {
+  src: string;
+  width: string;
+  height: string;
 }
 
 // NOTE: This is a prototype of an image wrapper that would be used in an image magnifying component
 export default function FloweryImage(props: FloweryImageProps) {
-
-  return (
-    <Image src={props.src} width={props.width} height={props.height} style={{ objectFit: 'contain' }}/>
-  );
+  return <Image src={props.src} width={props.width} height={props.height} style={{ objectFit: 'contain' }} />;
 }
