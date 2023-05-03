a77be18edb4cf7a0b6d49cbb62cb1a75dc59e666
szmarkiewicz <markiewiczszym@gmail.com>
2023-05-03 03:40:54 +0200

    Implement mock server for tests

--- flower-shop-frontend/src/utils/renderWithRouter.tsx
+++ flower-shop-frontend/src/utils/renderWithRouter.tsx
@@ -1,12 +1,14 @@
-import {render} from "@testing-library/react";
-import {BrowserRouter} from "react-router-dom";
-import userEvent from '@testing-library/user-event'
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
-export const renderWithRouter = (ui : JSX.Element, {route = '/'} = {}) => {
-  window.history.pushState({}, 'Test page', route)
+export const renderWithRouter = (ui : JSX.Element, {route = '/', state = {}} = {}) => {
+  window.history.pushState(state, 'Test page', route);
 
   return {
     user: userEvent,
-    ...render(ui, {wrapper: BrowserRouter}),
-  }
-}
+    ...render(ui, { wrapper: BrowserRouter })
+  };
+};
+
+export default renderWithRouter;
