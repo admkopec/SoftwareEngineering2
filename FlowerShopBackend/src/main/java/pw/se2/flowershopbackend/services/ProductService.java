b82a1f0c1c49551ae94b07944626ba0659afcc06
Adam Kopeć <adamkopec5@icloud.com>
2023-05-12 22:07:00 +0200

    Made a more elegant solution for the fix

--- FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/services/ProductService.java
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/services/ProductService.java
@@ -109,11 +109,6 @@ public class ProductService {
                                              Integer minPrice,
                                              Integer maxPrice,
                                              Pageable page) {
-        if (query == null) {
-            query = "%%";
-        } else {
-            query = "%"+query+"%";
-        }
         if (minPrice == null) {
             minPrice = Integer.MIN_VALUE;
         }
@@ -122,7 +117,7 @@ public class ProductService {
         }
         if (maxPrice < minPrice)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid min/max prices.");
-        return productRepository.findByNameLikeIgnoreCaseAndCategoryInAndPriceBetween(query, getCategoriesFrom(categories), Double.valueOf(minPrice), Double.valueOf(maxPrice), page);
+        return productRepository.findByNameContainsIgnoreCaseAndCategoryInAndPriceBetween(query, getCategoriesFrom(categories), Double.valueOf(minPrice), Double.valueOf(maxPrice), page);
     }
 
     private List<Product.Category> getCategoriesFrom(String categoryString) {
