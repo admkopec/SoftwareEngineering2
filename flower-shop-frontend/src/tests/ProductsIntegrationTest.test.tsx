5380b5e5fa5d9e6fb4718f56081ea23d5e30b65f
Alex <aleksandranovak.contact@gmail.com>
2023-05-24 22:21:49 +0200

    Integration tests

---
+++ flower-shop-frontend/src/tests/ProductsIntegrationTest.test.tsx
@@ -0,0 +1,7 @@
+import {regions} from "../components/Footer";
+import {getBackendURL} from "../services/user.service";
+import {JWTToken} from "../resources/types";
+
+test('testing product getting with Team 2', () => {
+
+});
