2fcb38420a10f7e45dc6950261982b05758ab88b
Adam Kopeć <adamkopec5@icloud.com>
2023-05-26 18:29:53 +0200

    Fixed integration tests to accept sleeping team 2

--- flower-shop-frontend/src/tests/ProductsIntegrationTest.test.tsx
+++ flower-shop-frontend/src/tests/ProductsIntegrationTest.test.tsx
@@ -1,20 +1,23 @@
 import {regions} from "../components/Footer";
 import {getBackendURL} from "../services/user.service";
-import {JWTToken, Product} from "../resources/types";
+import {Product} from "../resources/types";
+
 const fetch = require('node-fetch');
 
 // When there are no products, 404 is returned instead of an empty array, so the test is not passing
 
-// test('testing product getting with Team 2', () => {
-//     sessionStorage.setItem('backendURL', regions.alpsMountains);
-//     return fetch(`${getBackendURL() }/api/products/`).then((response: Response) => {
-//         if (response.ok) return response.json();
-//         throw new Error(`ERROR ${response.status}`);
-//     })
-//         .then((products: Product[]) => {
-//             expect(products.length).toBeGreaterThanOrEqual(0)
-//         })
-// });
+test('testing product getting with Team 2', () => {
+    sessionStorage.setItem('backendURL', regions.alpsMountains);
+    return fetch(`${getBackendURL() }/api/products/`).then((response: Response) => {
+        if (response.ok) return response.json();
+        throw new Error(`ERROR ${response.status}`);
+    })
+        .then((products: Product[]) => {
+            expect(products.length).toBeGreaterThanOrEqual(0)
+        }).catch((error) => {
+            expect(error.message).toMatch(/ERROR (503)|(404)/);
+        })
+});
 
 interface team3Token {token: string}
 test('testing product getting with Team 3', () => {
