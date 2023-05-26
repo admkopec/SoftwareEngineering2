d63f82537178324672a3db12fec8f30999595deb
Adam Kopeć <adamkopec5@icloud.com>
2023-05-26 18:16:59 +0200

    Fixed an overlook model return inside a dto

--- FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/BasketDto.java
+++ FlowerShopBackend/src/main/java/pw/se2/flowershopbackend/web/BasketDto.java
@@ -1,13 +1,12 @@
 package pw.se2.flowershopbackend.web;
 
 import pw.se2.flowershopbackend.models.BasketItem;
-import pw.se2.flowershopbackend.models.Product;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-public record BasketDto(@NotNull Product product, @Positive int quantity) {
+public record BasketDto(@NotNull ProductDto product, @Positive int quantity) {
     public static BasketDto valueFrom(BasketItem basket) {
-        return new BasketDto(basket.getProduct(), basket.getQuantity());
+        return new BasketDto(ProductDto.valueFrom(basket.getProduct()), basket.getQuantity());
     }
 }
