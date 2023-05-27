7b542061fd646f80c63771d2912577810ac75908
Adam Kopeć <adamkopec5@icloud.com>
2023-05-27 14:16:47 +0200

    Fixed some issues with images and buttons being behind elements (#109)

--- flower-shop-frontend/src/components/FloweryImage.tsx
+++ flower-shop-frontend/src/components/FloweryImage.tsx
@@ -5,9 +5,10 @@ interface FloweryImageProps {
   src: string;
   width: string;
   height: string;
+  style?: React.CSSProperties;
 }
 
 // NOTE: This is a prototype of an image wrapper that would be used in an image magnifying component
 export default function FloweryImage(props: FloweryImageProps) {
-  return <Image src={props.src} width={props.width} height={props.height} duration={2000} style={{ objectFit: 'contain' }} />;
+  return <Image src={props.src} width={props.width} height={props.height} duration={2000} style={props.style ?? { objectFit: 'contain' }} />;
 }
--- flower-shop-frontend/src/components/ProductCard.tsx
+++ flower-shop-frontend/src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import Card from '@mui/material/Card';
-import { CardMedia, SxProps, Theme } from '@mui/material';
+import {Box, SxProps, Theme} from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
@@ -7,9 +7,10 @@ import Button from '@mui/material/Button';
 import React from 'react';
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 import Paper from '@mui/material/Paper';
-import { useNavigate } from 'react-router-dom';
-import { Product } from '../resources/types';
-import { SurfaceSizes } from '../resources/constants';
+import {useNavigate} from 'react-router-dom';
+import {Product} from '../resources/types';
+import {SurfaceSizes} from '../resources/constants';
+import FloweryImage from "./FloweryImage";
 
 interface ProductCardProps {
   product: Product;
@@ -27,14 +28,10 @@ export default function ProductCard(props: ProductCardProps & React.ComponentPro
   return (
     <Paper elevation={4} sx={{ width: 'fit-content', height: 'fit-content', ...props.sx }}>
       <Card sx={{ width: props.size, height: 'auto' }}>
-        <CardMedia
-          component="img"
-          width="auto"
-          height={props?.size ? props.size*0.6 : "auto"}
-          sx={{ objectFit: 'contain', p: 2, maxWidth: props.size }}
-          alt={`a picture of a ${props.product.name}`}
-          image={`data:image/png;base64,${props.product.image}`}
-        />
+        <Box sx={{p: 2}}/>
+        <FloweryImage src={`data:image/png;base64,${props.product.image || ''}`} width="auto"
+                      height={props?.size ? `${(props.size*0.6).toString()}px`: "auto"}
+                      style={{ objectFit: 'contain', padding: 2, maxWidth: props.size }}/>
         <CardContent sx={{display: 'flex', flexDirection: 'column', flexWrap: 'nowrap', justifyContent: 'space-evenly'}}>
           <Typography variant="h5" component="div" role="product name">
             {props.product.name}
@@ -44,11 +41,14 @@ export default function ProductCard(props: ProductCardProps & React.ComponentPro
           </Typography> }
           <Typography variant="body2">{props.product.description}</Typography>
           { props.isDetailed && <Typography sx={{ p: 1 }} variant='h5' textAlign={'center'}>
-            {props.product.price}.00 $
+            {props.product.price.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })}
           </Typography> }
         </CardContent>
         <CardActions>
-          <Button size="small" endIcon={<KeyboardArrowRightRoundedIcon />} onClick={goToProduct}>
+          <Button size="small" style={{zIndex: 50}} endIcon={<KeyboardArrowRightRoundedIcon />} onClick={goToProduct}>
             Go to Product
           </Button>
         </CardActions>
