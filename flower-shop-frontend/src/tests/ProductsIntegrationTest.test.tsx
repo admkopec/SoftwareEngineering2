a7ac2056b3c3710967516beb0e6ad0773165a115
Adam Kopeć <adamkopec5@icloud.com>
2023-06-16 16:34:48 +0200

    Changed some integration tests

--- flower-shop-frontend/src/tests/ProductsIntegrationTest.test.tsx
+++ flower-shop-frontend/src/tests/ProductsIntegrationTest.test.tsx
@@ -1,6 +1,5 @@
 import {regions} from "../resources/constants";
 import {Product} from "../resources/types";
-import {loginWithCredentials} from "../services/user.service";
 import {fetchProductsFiltered} from "../services/product.service";
 import {sleepingBackendWrapper} from "./Helpers.integration";
 
@@ -15,7 +14,7 @@ test('testing product getting with Our Backend', () => {
 
 // Team 2 is sometimes sleeping so accept the test if we get 503
 // When there are no products, 404 is returned instead of an empty array, so this has to be handled in catch
-test.skip('testing product getting with Team 2', () => {
+test('testing product getting with Team 2', () => {
     sessionStorage.setItem('backendURL', regions.alpsMountains);
     /* eslint-disable jest/no-conditional-expect */
     return sleepingBackendWrapper(() =>
@@ -30,12 +29,13 @@ test.skip('testing product getting with Team 2', () => {
     /* eslint-enable jest/no-conditional-expect */
 });
 
-test.skip('testing product getting with Team 3', () => {
+test('testing product getting with Team 3', () => {
     sessionStorage.setItem('backendURL', regions.easterIsland);
-    return loginWithCredentials({username: 'admin@admin.com', password: 'admin'}).then(() =>
-        // @ts-ignore Arguments
-        fetchProductsFiltered().then((products: Product[]) => {
-            expect(products.length).toBeGreaterThanOrEqual(0)
-        })
-    )
+    // @ts-ignore Arguments
+    return fetchProductsFiltered().then((products: Product[]) => {
+        expect(products.length).toBeGreaterThanOrEqual(0)
+    }).catch((error: Error) => {
+        // Team 3 may still require Auth to view products
+        expect(error.message).toBe('ERROR 401');
+    })
 });
